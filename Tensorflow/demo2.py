1a0883e8d6620184e0651c8d5653ff23c2904516
David <newhwdy@gmail>
2020-04-04 07:37:28 +0900

    new

--- Tensorflow/demo2.py
+++ Tensorflow/demo2.py
@@ -1,2 +1,17 @@
-import numpy as np
 import tensorflow as tf
+# session 控制
+matrix1 = tf.constant([[3, 3]])
+matrix2 = tf.constant([[2], [2]])
+
+product = tf.matmul(matrix1, matrix2)
+
+##method = 1
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+sess.close()
+
+##method2
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result2)
